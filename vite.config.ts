88777d209d94b8eb6b9192511608cfc88f7099a0
Pradip Singh <kumarpradip3956@gmail.com>
2025-02-09 11:40:01 +0545

    add: ts and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      routes: path.resolve(__dirname, "./src/routes"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      components: path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
