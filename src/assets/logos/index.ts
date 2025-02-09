0f28f2f1b01a3476efb98bf9c042faddc9005aff
Pradip Singh <kumarpradip3956@gmail.com>
2025-02-09 11:19:43 +0545

    chore: code cleanup and icons add

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,7 @@
+import Fevicon from "./fevicon.png";
+import FaviconWithBackground from "./favicon-with-white-bg.png";
+import LogoJpg from "./logo-jpg.jpg";
+import LogoPng from "./logo-png.png";
+import UserIcon from "./use-placeholder.jpg";
+
+export { FaviconWithBackground, Fevicon, LogoJpg, LogoPng, UserIcon };
